ddb8e7e9b483494bef4ea6e1332e5ecb5c6502cf
Afiq <afiqvdx20@gmail.com>
2023-01-24 16:18:15 +0800

    Select all drum class

---
+++ index.js
@@ -0,0 +1,11 @@
+var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfDrumsButtons; i++){
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("Clicked");
+    });
+    
+}
+
+
